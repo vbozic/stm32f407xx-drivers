1b416be06d9136a32b228c697344d9cdd3261d07
vbozic <vbozic@3plus.hr>
2024-09-17 22:52:23 +0200

    Clock disable Macros for GPIOx peripheral

--- Drivers/Inc/stm32f407xx.h
+++ Drivers/Inc/stm32f407xx.h
@@ -182,4 +182,18 @@ typedef struct
 
 #define SYSCFG_PCLK_EN()		(RCC->APB2ENR |= (1 << 14))
 
+/*
+ * Clock disable Macros for GPIOx peripherals
+ */
+
+#define GPIOA_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 0))
+#define GPIOB_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 1))
+#define GPIOC_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 2))
+#define GPIOD_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 3))
+#define GPIOE_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 4))
+#define GPIOF_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 5))
+#define GPIOG_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 6))
+#define GPIOH_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 7))
+#define GPIOI_PCLK_DI()			(RCC->AHB1ENR &= ~(1 << 8))
+
 #endif /* INC_STM32F407XX_H_ */

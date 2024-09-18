d99379f165fef0ec1a473214250a058185159011
vbozic <vbozic@3plus.hr>
2024-09-18 20:07:47 +0200

    Add driver supported APIs for peripheral clock, gpio init and deinit, data read and write and IRQ

--- Drivers/Inc/stm32f407xx_gpio_driver.h
+++ Drivers/Inc/stm32f407xx_gpio_driver.h
@@ -31,4 +31,41 @@ typedef struct
 	GPIO_PinConfig_t GPIO_PinConfig;
 }GPIO_Handle_t;
 
+
+/*******************************************************************************************************************************
+ * 											APIs supported by this driver
+ * 						For more information about the APIs check the function definition
+ ******************************************************************************************************************************/
+
+/*
+ * Peripheral Clock Control
+ */
+
+void GPIO_PeriClockControl(void);
+
+/*
+ * GPIO Init and Deinit
+ */
+
+void GPIO_Init(void);
+void GPIO_DeInit(void);
+
+/*
+ * Data Read and Write
+ */
+
+void GPIO_ReadFromInputPin(void);
+void GPIO_ReadFromInputPort(void);
+void GPIO_WriteToOutputPin(void);
+void GPIO_WriteToOutputPort(void);
+void GPIO_ToggleOutputPin(void);
+
+/*
+ * IRQ Configuration and ISR handling
+ */
+void GPIO_IRQConfig(void);
+void GPIO_IRQHandling(void);
+
+
+
 #endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */

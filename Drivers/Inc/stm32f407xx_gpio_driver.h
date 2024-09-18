131fb931987988577ffef45244117944f89e5afc
vbozic <vbozic@3plus.hr>
2024-09-18 19:50:30 +0200

    Add GPIO Handle structure and GPIO Pin Config structure

--- Drivers/Inc/stm32f407xx_gpio_driver.h
+++ Drivers/Inc/stm32f407xx_gpio_driver.h
@@ -10,4 +10,25 @@
 
 #include "stm32f407xx.h"
 
+
+typedef struct
+{
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOPType;
+	uint8_t GPIO_PinAltFunMode;
+}GPIO_PinConfig_t;
+
+/*
+ * A handle structure for a GPIO pin
+ */
+
+typedef struct
+{
+	GPIO_RegDef_t *pGPIOx;						/* This holds the base address of the GPIO port to which the pin belongs */
+	GPIO_PinConfig_t GPIO_PinConfig;
+}GPIO_Handle_t;
+
 #endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */

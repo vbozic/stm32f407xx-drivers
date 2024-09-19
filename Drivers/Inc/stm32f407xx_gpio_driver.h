283ada26e0c8f4bfd6fba2a07b9dcceffeaf61c1
vbozic <vbozic@3plus.hr>
2024-09-19 21:52:38 +0200

    Add GPIO_PinConfig_t structure parameter definition

--- Drivers/Inc/stm32f407xx_gpio_driver.h
+++ Drivers/Inc/stm32f407xx_gpio_driver.h
@@ -14,14 +14,37 @@
 typedef struct
 {
 	uint8_t GPIO_PinNumber;
-	uint8_t GPIO_PinMode;
-	uint8_t GPIO_PinSpeed;
-	uint8_t GPIO_PinPuPdControl;
-	uint8_t GPIO_PinOPType;
-	uint8_t GPIO_PinAltFunMode;
+	uint8_t GPIO_PinMode;							/* possible values from @GPIO_PIN_MODES */
+	uint8_t GPIO_PinSpeed;							/* possible values from @GPIO_PIN_SPEED */
+	uint8_t GPIO_PinPuPdControl;					/* possible values from @GPIO_PIN_PUPD */
+	uint8_t GPIO_PinOPType;							/* possible values from @GPIO_PIN_OP_TYPE */
+	uint8_t GPIO_PinAltFunMode;						/* possible values from @GPIO_PIN_ALT_MODE */
 }GPIO_PinConfig_t;
 
 /*
+ * Define GPIO pin numbers
+ */
+
+#define GPIO_PIN_NO_0					0
+#define GPIO_PIN_NO_1					1
+#define GPIO_PIN_NO_2					2
+#define GPIO_PIN_NO_3					3
+#define GPIO_PIN_NO_4					4
+#define GPIO_PIN_NO_5					5
+#define GPIO_PIN_NO_6					6
+#define GPIO_PIN_NO_7					7
+#define GPIO_PIN_NO_8					8
+#define GPIO_PIN_NO_9					9
+#define GPIO_PIN_NO_10					10
+#define GPIO_PIN_NO_11					11
+#define GPIO_PIN_NO_12					12
+#define GPIO_PIN_NO_13					13
+#define GPIO_PIN_NO_14					14
+#define GPIO_PIN_NO_15					15
+
+
+/*
+ * @GPIO_PIN_MODES
  * GPIO pin possible modes
  */
 

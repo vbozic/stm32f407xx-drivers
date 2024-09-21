44f24d4fe6503db1aad2888c956ed5ba80dc4dc6
vbozic <vbozic@3plus.hr>
2024-09-21 19:28:26 +0200

    Add debug files

--- Debug/Drivers/Src/stm32f407xx_gpio_driver.d
+++ Debug/Drivers/Src/stm32f407xx_gpio_driver.d
@@ -1,8 +1,11 @@
 Drivers/Src/stm32f407xx_gpio_driver.o: \
  ../Drivers/Src/stm32f407xx_gpio_driver.c \
  C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx_gpio_driver.h \
- C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx.h
+ C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx.h \
+ C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx_gpio_driver.h
 
 C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx_gpio_driver.h:
 
 C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx.h:
+
+C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx_gpio_driver.h:
--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,4 +1,10 @@
 Src/main.o: ../Src/main.c \
+ C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx.h \
+ C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx_gpio_driver.h \
  C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx.h
+
+C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx.h:
+
+C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx_gpio_driver.h:
 
 C:/git-projects/stm32f407xx-drivers/Drivers/Inc/stm32f407xx.h:

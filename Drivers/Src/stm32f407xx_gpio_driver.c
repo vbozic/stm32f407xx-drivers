41b0123cb6769651b3a0898b89413bc52fb90046
vbozic <vbozic@3plus.hr>
2025-03-16 19:19:55 +0100

    chore: Add definition for configuring the GPIO port selection in SYSCFG_EXTICR

--- Drivers/Src/stm32f407xx_gpio_driver.c
+++ Drivers/Src/stm32f407xx_gpio_driver.c
@@ -123,6 +123,11 @@ void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 		}
 
 		// 2. configure the GPIO port selection in SYSCFG_EXTICR
+		uint8_t temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 4;
+		uint8_t temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 4;
+		uint8_t portcode = GPIO_BASEADDR_TO_CODE(pGPIOHandle->pGPIOx);
+		SYSCFG_PCLK_EN();
+		SYSCFG->EXTICR[temp1] = portcode << (temp2 * 4);
 
 		// 3. enable the EXTI interrupt delivery using IMR
 		EXTI->IMR |= 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber;

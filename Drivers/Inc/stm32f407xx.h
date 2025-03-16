fe8071e8e319d261d8830ff82740fdbd62e5c749
vbozic <vbozic@3plus.hr>
2025-03-16 19:18:32 +0100

    feature: add IRQ defines
    processor specific details for set and clear interrupt, IRQ numbers for MCU, define that returns port code for giver GPIOx base address

--- Drivers/Inc/stm32f407xx.h
+++ Drivers/Inc/stm32f407xx.h
@@ -12,6 +12,34 @@
 
 #define __vo volatile
 
+/**************** START: Processor Specific Details ****************
+ * ARM Cortex Mx Processor NVIC ISERx (Interrupt Set) register Adresses
+ */
+
+#define NVIC_ISER0			((__vo uint32_t*)0xE000E100)
+#define NVIC_ISER1			((__vo uint32_t*)0xE000E104)
+#define NVIC_ISER2			((__vo uint32_t*)0xE000E108)
+#define NVIC_ISER3			((__vo uint32_t*)0xE000E10C)
+
+/*
+ * ARM Cortex Mx Processor NVIC ICERx (Interrupt Clear) register Adresses
+ */
+
+#define NVIC_ICER0			((__vo uint32_t*)0xE000E180)
+#define NVIC_ICER1			((__vo uint32_t*)0xE000E184)
+#define NVIC_ICER2			((__vo uint32_t*)0xE000E188)
+#define NVIC_ICER3			((__vo uint32_t*)0xE000E18C)
+
+/*
+ * ARM Cortex Mx Processor Priority Register Address Calculation
+ */
+#define NVIC_PR_BASE_ADDR	((__vo uint32_t*)0xE000E400)
+
+/*
+ * ARM Cortex Mx Processor number of priority bits implemented in Priority Register
+ */
+#define NO_PR_BITS_IMPLEMENTED		4
+
 /*
  * Base addresses of FLASH and SRAM memories
  */
@@ -156,7 +184,9 @@ typedef struct
 
 #define RCC									((RCC_RegDef_t*)RCC_BASEADDR)
 
-#define EXTI								((RCC_RegDef_t*)EXTI_BASEADDR)
+#define EXTI								((EXTI_RegDef_t*)EXTI_BASEADDR)
+
+#define SYSCFG								((SYSCFG_RegDef_t*)SYSCFG_BASEADDR)
 
 /*
  * Clock enable Macros for GPIOx peripherals
@@ -265,6 +295,31 @@ typedef struct
 #define GPIOG_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 6)); (RCC->AHB1RSTR &= ~(1 << 6)); } while(0)
 #define GPIOH_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 7)); (RCC->AHB1RSTR &= ~(1 << 7)); } while(0)
 #define GPIOI_REG_RESET()		do{ (RCC->AHB1RSTR |= (1 << 8)); (RCC->AHB1RSTR &= ~(1 << 8)); } while(0)
+
+/*
+ * returns port code for given GPIOx base address
+ */
+#define GPIO_BASEADDR_TO_CODE(x)	(	(x == GPIOA) ? 0 :\
+										(x == GPIOB) ? 1 :\
+										(x == GPIOC) ? 2 :\
+										(x == GPIOD) ? 3 :\
+										(x == GPIOE) ? 4 :\
+										(x == GPIOF) ? 5 :\
+										(x == GPIOG) ? 6 :\
+										(x == GPIOH) ? 7 :\
+										(x == GPIOI) ? 8 :0		)
+
+/*
+ * IRQ (Interrupt Request) Numbers of STM32F407x MCU
+ */
+#define IRQ_NO_EXTI0		6
+#define IRQ_NO_EXTI1		7
+#define IRQ_NO_EXTI2		8
+#define IRQ_NO_EXTI3		9
+#define IRQ_NO_EXTI4		10
+#define IRQ_NO_EXTI9_5		23
+#define IRQ_NO_EXTI15_10	40
+
 
 /*
  * Some generic macros
